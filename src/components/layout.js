4951a7a1a0a9670b4f58dcc120fa0550b40092c3
Pablo Garcia <pgarciacamou@gmail.com>
2019-03-07 22:05:26 -0700

    add background image

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,8 @@ import { StaticQuery, graphql } from 'gatsby';
 
 // import Header from './header';
 import Footer from './footer';
-import './layout.css';
+import '../styles/layout.css';
+import '../styles/background.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,22 +25,24 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
+    render={
+      (/*data*/) => (
+        <>
+          {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+          </div>
           <Footer />
-        </div>
-      </>
-    )}
+        </>
+      )
+    }
   />
 );
 
