530f18372cfa544a5e5b9a306373923a7adeba2f
Pablo Garcia <pgarciacamou@gmail.com>
2019-04-03 15:22:15 -0700

    clean out code

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,19 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Header = ({ siteTitle, lead = '' }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      {lead !== '' && <p className="lead">{lead}</p>}
-    </div>
+  <header>
+    <h1 style={{ margin: 0 }}>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
+      </Link>
+    </h1>
+    {lead !== '' && <p className="lead">{lead}</p>}
   </header>
 );
 
