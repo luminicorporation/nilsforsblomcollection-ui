3f268de0d7f4da73c1d93a453a2df71104e1342f
Pablo Garcia <pgarciacamou@gmail.com>
2019-04-05 14:17:44 -0700

    Revert "use jpeg 2000 (jpf) version of NFC_Artwork_Desktop_01"
    This reverts commit 24ff78d5dfa775e3024ae5b9f79b7a958361ba12.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import headerImage from '../images/NFC_Artwork_Desktop_01.jpf';
+import headerImage from '../images/NFC_Artwork_Desktop_01.png';
 import '../styles/index.scss';
 
 const IndexPage = () => (
